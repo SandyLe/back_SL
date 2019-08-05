14fd4de2294d19085b629d56fb77affd76a170c2
lixianting <li_xianting@163.com>
2019-08-05 18:05:08 +0800

    登录

--- src/api/user.js
+++ src/api/user.js
@@ -2,8 +2,7 @@ import axios from '@/libs/api.request'
 import crypto from 'crypto'
 
 export const login = ({ userName, password }) => {
-  let pass = getMD5(password + 'b492f97ca837625b3d2ad39fdd5518ed')
-  console.log(pass)
+  let pass = decode(password)
   const data = {
     'username': userName,
     'password': pass
@@ -19,10 +18,17 @@ export const login = ({ userName, password }) => {
   })
 }
 
-export const getMD5 = (pass) => {
+/* export const getMD5 = (pass) => {
   let md5 = crypto.createHash('md5')
   md5.update(pass)
   return md5.digest('hex')
+} */
+
+export const decode = (pass) => {
+  const cipher = crypto.createCipher('aes-128-ecb', 'Password!')
+  var crypted = cipher.update(pass, 'utf8', 'hex')
+  crypted += cipher.final('hex')
+  return crypted
 }
 
 export const getUserInfo = (token) => {
@@ -95,3 +101,15 @@ export const restoreTrash = msg_id => {
     }
   })
 }
+/**
+ * aes解密
+ * @param data
+ * @param secretKey
+ * @returns {*}
+
+function aesDecrypt (encrypted, key) {
+  const decipher = crypto.createDecipher('aes-128-ecb', key)
+  var decrypted = decipher.update(encrypted, 'hex', 'utf8')
+  decrypted += decipher.final('utf8')
+  return decrypted
+} */
